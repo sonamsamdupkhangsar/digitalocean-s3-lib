e9f038945ee9c087e54ec0a3be21bc27f412739b
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2024-11-14 08:46:11 -0700

    udapte repo name in iml files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "sonam-s3-lib"
+rootProject.name = "digitalocean-s3-lib"
 include("lib")
